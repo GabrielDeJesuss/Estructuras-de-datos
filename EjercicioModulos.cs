1c6074f86b7d01826b9eb6740b5226af7c6d4ca3
GabrielDeJesuss <gmedina@unicaribe.edu.co>
2026-08-28 13:41:08 -0500

    Create EjercicioModulos.cs

---
+++ EjercicioModulos.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjericioModulos
+{
+    internal class Program
+    {
+        //procedimiento sumar e imprimir
+        public static void ImprimirSuma(int a, int b) { //a, b son los parametros
+            int suma = a + b;
+            Console.WriteLine (suma);
+        }
+
+        public static void MostrarPotenciaX(double num1, double potencia) 
+        {
+            double result = Math.Pow(num1, potencia);
+            Console.WriteLine ($"La potencia de {num1} elevado a {potencia} es: {result}");
+        }
+        static void Main(string[] args)
+        {
+            int x = 12, y = 3;
+
+            ImprimirSuma(y, x); //y, x (Argumenos actuales)
+            ImprimirSuma(1000, -1); //1000, -1 (Argumentos literales)
+            MostrarPotenciaX(x, y);
+        }
+    }
+}
