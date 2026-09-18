2c232acfc456b0b1f65878b24e53d0b9ebc4690e
GabrielDeJesuss <gmedina@unicaribe.edu.co>
2026-09-18 13:01:49 -0500

    Create vectores.cs

---
+++ vectores.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vectores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[5]
+                {25, 43, 80, 12, 14};
+            Console.WriteLine($"Elemento en [0]: {numeros[0]}");
+        }
+    }
+}
